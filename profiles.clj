4fc66122a24f9069d08764d1a7f15c0d88306734
Hugo Duncan <hugo@hugoduncan.org>
2014-03-25 10:01:22 -0400

    Add travis files and configuration

--- profiles.clj
+++ profiles.clj
@@ -13,7 +13,10 @@
        :checkout-deps-shares [:source-paths :test-paths :resource-paths
                               :compile-path]
        :plugins [[codox/codox.leiningen "0.6.4"]
-                 [lein-marginalia "0.7.1"]]}
+                 [lein-marginalia "0.7.1"]
+                 [lein-pallet-release "0.1.3"]]
+       :pallet-release {:url "https://pbors:${GH_TOKEN}@github.com/pallet/awaze.git",
+                        :branch "master"}}
  :gen {:prep-tasks ^:replace []}
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
        :codox {:sources ["src" "target/generated"]
@@ -29,4 +32,5 @@
   :set-version
   {:updates [{:path "README.md" :no-snapshot true}]}}
  :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
- :clojure-1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
+ :clojure-1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+ :clojure-1.6 {:dependencies [[org.clojure/clojure "1.6.0-RC.4"]]}}
