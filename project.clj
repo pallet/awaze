5ceda0f0e7fe2cffba27195b3d53411db8f46a2b
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-09-12 12:59:26 +0100

    Update to AWS SDK 1.8.3
    AWS SDK 1.8.3 includes the t2.small and t2.medium instance types

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.amazonaws/aws-java-sdk "1.8.2"
+                 [com.amazonaws/aws-java-sdk "1.8.3"
                   :exclusions [commons-logging commons-codec joda-time]]
                  [fipp "0.4.3"]
                  [joda-time "2.2"]]
