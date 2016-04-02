cdcc946185c088d6971d724444ebb95a3a6f4942
Hugo Duncan <hugo@hugoduncan.org>
2016-04-01 21:00:55 -0400

    Update to fipp 0.6.4

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
-{:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+{:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                       [ch.qos.logback/logback-classic "1.1.1"]
                       [org.slf4j/jcl-over-slf4j "1.7.5"]
                       [org.clojure/tools.logging "0.2.6"]
-                      [fipp "0.4.3"]]
+                      [fipp "0.6.4"]]
        :source-paths ["src" "dev-src" "target/generated"]
        :plugins [[lein-pallet-release "LATEST"]]}
  :gen {:prep-tasks ^:replace []}
